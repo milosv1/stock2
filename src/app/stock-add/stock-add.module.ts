c78b7e2d791e98b52e2e05a8888fee15301eedef
Milos Vuksanovic <milosvuksanovic@Miloss-MacBook-Pro.local>
2019-06-16 14:59:37 +1000

    changed things on stockadd+modulets

--- src/app/stock-add/stock-add.module.ts
+++ src/app/stock-add/stock-add.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { StockAddPage } from './stock-add.page';
@@ -24,6 +24,7 @@ const routes: Routes = [
     FormsModule,
     IonicModule,
     HttpClientModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes)
   ],
   declarations: [StockAddPage]
